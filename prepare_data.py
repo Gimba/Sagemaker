1527e8760003567189e86afa2258de836f933a6c
Martin Rosellen <martin.rosellen.16@ucl.ac.uk>
2020-03-08 13:12:17 +0000

    added min and max value to file name

--- prepare_data.py
+++ prepare_data.py
@@ -24,15 +24,18 @@ def main():
     data = pd.DataFrame()
     print(data.shape)
     for path in Path('.').rglob('training_data.csv'):
+
         data = data.append(pd.read_csv(path, header=None, index_col=None))
         print(data.shape)
-
+    min = data.min()
+    max = data.max()
     data -= data.min()
     data /= data.max()
     # shuffle data
     data = data.sample(frac=1)
 
-    data.to_csv('training_bside_metrics_energy.csv', index=None, header=None)
+    data.to_csv('training_bside_metrics_energy_min(' + str(min) + ')_max(' + str(max) + ').csv', index=None, 
+                header=None)
 
 
 if __name__ == '__main__':
